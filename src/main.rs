b6517734174e6bfd1d139202b6185de80fc28fa3
Thomas Johnson <NTmatter@gmail.com>
2025-05-28 13:43:12 +0200

    wip: Add placeholder main

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+
+//! Placeholder main, look at bins and lib for now.
+
+fn main() -> anyhow::Result<()> {
+    println!("RAWR - Reimplement and Watch Revisions!");
+    Ok(())
+}
