d23c49a2e8283581125fd2e1f0542ddc1e2def2a
Thomas Johnson <NTmatter@gmail.com>
2025-07-01 21:36:25 +0200

    wip(compare): Database cleanup

--- src/rawr.sql
+++ src/rawr.sql
@@ -2,7 +2,7 @@
 
 -- Table: Upstream Codebase
 -- Describes a codebase of interest
-CREATE TABLE IF NOT EXISTS codebase
+CREATE TABLE IF NOT EXISTS upstream
 (
     id            INT  NOT NULL PRIMARY KEY,
     name          TEXT NOT NULL UNIQUE,
@@ -15,49 +15,45 @@ CREATE TABLE IF NOT EXISTS codebase
 -- No primary key due to high chance of duplicates.
 CREATE TABLE IF NOT EXISTS upstream
 (
-    -- Friendly name of upstream codebase.
+    -- ID of upstream codebase.
     -- Use ID later.
-    codebase       TEXT    NOT NULL,
+    upstream      TEXT    NOT NULL,
 
     -- Treeish of revision, expecting SHA1, may be anything.
-    revision       TEXT    NOT NULL,
+    revision      TEXT    NOT NULL,
 
     -- Relative path to containing file.
-    path           TEXT    NOT NULL,
+    file          TEXT    NOT NULL,
 
-    -- Byte Offset of item within file.
-    start_byte     INTEGER NOT NULL,
+    start_byte    INTEGER NOT NULL,
+    end_byte      INTEGER NOT NULL,
 
-    -- Length of matched item
-    length         INTEGER NOT NULL,
-
-    -- Name of matched item
-    identifier     TEXT    NOT NULL,
+    start_line    INTEGER NOT NULL,
+    start_column  INTEGER NOT NULL,
+    end_line      INTEGER NOT NULL,
+    end_column    INTEGER NOT NULL,
 
     -- Kind of matched item, extracted from matcher
-    kind           TEXT    NOT NULL,
+    kind          TEXT    NOT NULL,
 
-    -- Hash of matched bytes, selected algorithm. Likely SHA512.
-    hash_algorithm TEXT    NOT NULL,
-
-    -- Optional salt for hash. This will be problematic for lookups.
-    -- Store as u64.
-    salt           INT,
+    -- Name of matched item
+    identifier    TEXT    NOT NULL,
 
     -- Hash of matched bytes, stored as uppercase hex without leading 0x.
     -- Switch to BLOB for efficiency. Consider first 64 bits of SHA 512?
-    hash           TEXT    NOT NULL,
+    hash          BLOB    NOT NULL,
 
     -- Hash of matched bytes, after attempting to convert to valid UTF-8
     -- and stripping Unicode whitespace. This should aid in matching after
     -- reformatting.
-    hash_stripped  TEXT,
+    hash_stripped BLOB,
 
     -- Optional notes regarding item.
-    notes          TEXT,
+    notes         TEXT,
+
 
     -- Prevent duplicates of already-processed data.
-    CONSTRAINT UQ_upstream UNIQUE (codebase, revision, path, start_byte, length, identifier, kind, hash_algorithm, hash)
+    CONSTRAINT UQ_upstream UNIQUE (upstream, revision, file, start_byte, kind, identifier, hash)
 
 -- Skip FK for now, to simplify build.
 --     CONSTRAINT FK_upstream_codebase FOREIGN KEY (codebase)
@@ -66,10 +62,9 @@ CREATE TABLE IF NOT EXISTS upstream
 
 -- Index: Upstream primary query
 -- Expect intensive lookups across an item's history.
-CREATE INDEX IF NOT EXISTS IX_upstream ON upstream (codebase, path, identifier, kind, revision);
+CREATE INDEX IF NOT EXISTS IX_upstream_item_history ON upstream (codebase, path, identifier, kind, revision);
 
 -- Index: Hash Lookup
 -- Expect for lookups by hash, looking for duplicates by type, varying identifier,
 -- and across codebases.
-CREATE INDEX IF NOT EXISTS IX_upstream_hash ON upstream (codebase, hash, kind, identifier)
-    WHERE hash IS NOT NULL;
+CREATE INDEX IF NOT EXISTS IX_upstream_hash ON upstream (codebase, hash, kind, identifier);
